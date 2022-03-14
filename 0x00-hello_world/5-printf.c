7e400669d87c090b189bee7d57ed6c6029f7e1ee
Gaeygo <gaeygo@gmail.com>
2022-03-14 14:47:17 -0700

    corec

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-* main-this functions prints 
-*
-* Return: interger 0
-*/
+ * main-this functions prints 
+ *
+ * Return: interger 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
