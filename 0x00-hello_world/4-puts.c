c53e007e2075db45ffda1927c4f0e93b334e1118
Gaeygo <gaeygo@gmail.com>
2022-03-14 09:37:21 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+/**
+ * The main function returns a puts with an escaped apostrophe and returns 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
