473ddf252dbe23a74e6e8fef757819ed51919ecc
Gaeygo <gaeygo@gmail.com>
2022-03-14 14:31:45 -0700

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main- this functions prints 
+*
+* Description: 
+* Return: interger 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
